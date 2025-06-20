774cca357e443f8fadc4e5b77da54c0de01f5618
ianhorn <=>
2025-06-20 17:59:22 -0400

    config.js file

---
+++ stac-browser-config/config.js
@@ -0,0 +1,5 @@
+window.STAC_BROWSER_CONFIG = {
+  catalogTitle: 'KyFromAbove STAC Browser',
+  pathPrefix: '/browser/',
+  catalogURL: 'http://ec2-52-88-114-187.us-west-2.compute.amazonaws.com:8082/',
+};
