9b50639236917ae9d7d9436e5353ca91fdb13938
ianhorn <=>
2025-06-20 22:45:17 -0400

    remove stac - browser

--- stac-browser-dev/misc.sh
+++
@@ -1,7 +0,0 @@
-npm start -- --open --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/"
-
-npm run build -- --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/"
-
-npm run build -- \
---catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/" \
---historyMode=hash
--- stac-browser-dev/src/locales/ar/datepicker.js
+++
@@ -1,4 +0,0 @@
-const format = 'YYYY-MM-DD';
-const locale = import('vue2-datepicker/locale/ar');
-(await locale).default.formatLocale.firstDayOfWeek = 1;
-export default {format, locale};
